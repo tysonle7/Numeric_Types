d1c2895a6e9ce0fe65df40c14766f91ce7c02269
tysonle7 <125998611+tysonle7@users.noreply.github.com>
2023-03-09 09:16:26 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Numeric_Types
+{
+    class Program
+    {
+        static void Main ()
+        {
+            
+        Console.WriteLine("Length of rectangle: ");
+        double length = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Width of rectangle: ");
+        double width = double.Parse(Console.ReadLine());
+
+        double area = length * width;
+        Console.WriteLine("The area of rectangle is {0}", area);
+        }
+    }
+}
